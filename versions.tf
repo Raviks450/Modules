f6fb968bfb368bfcfa50208246c8ec3b39b1a57a
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-16 23:54:20 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  # Replace with your project ID
+  project = "qwiklabs-gcp-02-f72a2e32e8fe" 
+  # Replace with your desired region (e.g., "us-central1" or "EU")
+  region  = "us-central1"   
+}
