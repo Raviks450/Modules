90f51d0f5629982f3ae84fafecab67f1c31e78cb
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-16 23:38:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Create a new storage bucket
 resource "google_storage_bucket" "my_gcs_bucket" {
   # This name must be globally unique across all of Google Cloud
-  name          = "my-unique-bucket-name" 
-  location      = "US" # Location cannot be changed after creation
-  storage_class = "STANDARD" # e.g., STANDARD, NEARLINE, COLDLINE, ARCHIVE
+  name          = var.bucket-name
+  location      = var.location # Location cannot be changed after creation
+  storage_class = var.class # e.g., STANDARD, NEARLINE, COLDLINE, ARCHIVE
   
   # Optional: enables uniform bucket-level access for consistent permissions
   uniform_bucket_level_access = true 
